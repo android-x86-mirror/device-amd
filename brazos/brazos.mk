ed86a39b0a1afcabd018de51b23be4b43d5ede6f
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-12-09 16:35:25 +0800

    brazos.mk: remove unnecessary PRODUCT_COPY_FILES

--- brazos/brazos.mk
+++ brazos/brazos.mk
@@ -1,5 +1,4 @@
 PRODUCT_COPY_FILES := \
-    device/common/generic_x86/GenericTouch.idc:system/usr/idc/eGalax_Inc__USB_TouchController.idc \
     $(LOCAL_PATH)/AT_Translated_Set_2_keyboard.kl:system/usr/keylayout/AT_Translated_Set_2_keyboard.kl \
 
 $(call inherit-product,$(LOCAL_PATH)/../common/generic_amd.mk)
