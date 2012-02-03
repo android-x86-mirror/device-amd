39a217597e84ff78574b1aeb2bd30c93b4b03d76
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-02-03 21:04:26 +0800

    remove unnecessary code
    The init.sh is moved to generic_x86 for common usage.

--- common/generic_amd.mk
+++ common/generic_amd.mk
@@ -1,7 +1,6 @@
 PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/init.sh:system/etc/init.sh \
     $(LOCAL_PATH)/asound.conf:system/etc/asound.conf \
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
